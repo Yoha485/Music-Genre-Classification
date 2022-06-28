2b582f8109cf3c909d8ed4c370048c2eb306301c
Morozov Anton <antonmorozoff64@gmail.com>
2022-06-28 21:45:25 +0200

    Code Refactoring

--- backend/mapper.py
+++ backend/mapper.py
@@ -1,14 +1,14 @@
 def get_genre(index):
     genre_dictionary = {
-        0: 'jazz',
-        1: 'reggae',
+        0: 'jazz🎷',
+        1: 'reggae🌿',
         2: 'hiphop',
-        3: 'pop',
-        4: 'rock',
-        5: 'country',
-        6: 'classical',
-        7: 'disco',
-        8: 'metal',
-        9: 'blues'
+        3: 'pop✨',
+        4: 'rock🎸',
+        5: 'country🪕',
+        6: 'classical🎹',
+        7: 'disco🕺🏽',
+        8: 'metal🤘',
+        9: 'blues🥁'
     }
     return genre_dictionary[index]
